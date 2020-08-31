52f796f33578e3c07af727aa726c256d48c5e157
achzaky <zakyy23@gmail.com>
2020-08-31 13:22:24 +0700

    foto dan link

--- app/Views/home.php
+++ app/Views/home.php
@@ -41,21 +41,21 @@
                             d="M2.5 13.5A.5.5 0 0 1 3 13h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5zM2 2h12s2 0 2 2v6s0 2-2 2H2s-2 0-2-2V4s0-2 2-2z" />
                     </svg>Auditorium</a>
 
-                <a id='navRC' class="nav-link active" style="width: fit-content;">
+                   <a id='navRC' href="https://foreng.wika.co.id/news" target="__blank" class="nav-link active" style="width: fit-content;">
                     <svg width="1em" height="1em" viewBox="0 0 16 16" class="bi bi-cloud-arrow-down-fill"
                         fill="currentColor" xmlns="http://www.w3.org/2000/svg">
                         <path fill-rule="evenodd"
                             d="M8 2a5.53 5.53 0 0 0-3.594 1.342c-.766.66-1.321 1.52-1.464 2.383C1.266 6.095 0 7.555 0 9.318 0 11.366 1.708 13 3.781 13h8.906C14.502 13 16 11.57 16 9.773c0-1.636-1.242-2.969-2.834-3.194C12.923 3.999 10.69 2 8 2zm2.354 6.854l-2 2a.5.5 0 0 1-.708 0l-2-2a.5.5 0 1 1 .708-.708L7.5 9.293V5.5a.5.5 0 0 1 1 0v3.793l1.146-1.147a.5.5 0 0 1 .708.708z" />
                     </svg>Resource Center</a>
 
-                <a id='navAbout' href="https://foreng.wika.co.id/" target="__blank" class="nav-link active">
+                   <a id='navAbout' href="https://foreng.wika.co.id/about" target="__blank" class="nav-link active">
                     <svg width="1em" height="1em" viewBox="0 0 16 16" class="bi bi-info-circle-fill" fill="currentColor"
                         xmlns="http://www.w3.org/2000/svg">
                         <path fill-rule="evenodd"
                             d="M8 16A8 8 0 1 0 8 0a8 8 0 0 0 0 16zm.93-9.412l-2.29.287-.082.38.45.083c.294.07.352.176.288.469l-.738 3.468c-.194.897.105 1.319.808 1.319.545 0 1.178-.252 1.465-.598l.088-.416c-.2.176-.492.246-.686.246-.275 0-.375-.193-.304-.533L8.93 6.588zM8 5.5a1 1 0 1 0 0-2 1 1 0 0 0 0 2z" />
                     </svg>About</a>
 
-                <a id='navEF' class="nav-link active" style="width: fit-content;">
+                   <a id='navEF' class="nav-link active" style="width: fit-content;">
                     <svg width="1em" height="1em" viewBox="0 0 16 16" class="bi bi-pencil-square" fill="currentColor"
                         xmlns="http://www.w3.org/2000/svg">
                         <path
